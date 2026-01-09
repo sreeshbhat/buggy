8465cd58436f0ebd4fecb196399298a3251b5878
sreeshbhat <69189834+sreeshbhat@users.noreply.github.com>
2026-01-09 10:55:19 +0530

    Delete skill_extractor.py

--- skill_extractor.py
+++
@@ -1,8 +0,0 @@
-from data.skills import SKILLS_DB
-
-def extract_skills(resume_text):
-    found = []
-    for skill in SKILLS_DB:
-        if skill in resume_text:
-            found.append(skill)
-    return list(set(found))
